b971cf40b0e8c124fe12865b5f8b11f0a7048579
pvanvlaa <pvanvlaanderen@uchicago.edu>
2025-05-24 15:30:32 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import sys
+from PySide6 import QtWidgets, QtUiTools, QtCore
+from PySide6.QtCore import Qt
+
+def load_ui(ui_file):
+    loader = QtUiTools.QUiLoader()
+    ui_file = QtCore.QFile(ui_file)
+    ui_file.open(QtCore.QFile.ReadOnly)
+    window = loader.load(ui_file)
+    ui_file.close()
+    return window
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    main_win = load_ui("ui/main_window.ui")
+    main_win.show()
+    sys.exit(app.exec())
