cac1d1b3f168f4b885d7af2fe855d8f17c9fb27c
pvanvlaa <pvanvlaanderen@uchicago.edu>
2025-07-18 15:54:50 -0500

    Modified requirements.txt & wokring image takking

--- camera_optimization.py
+++ camera_optimization.py
@@ -5,20 +5,22 @@ import cv2
 vimba = Vimba.get_instance()
 
 def get_cams():
-    cams = vimba.get_all_cameras
-    if(cams):
-        print(f"Camera Found:{cams}")
-        return cams
-    else:
-        raise FileNotFoundError
-    
+    with vimba:
+        cams = vimba.get_all_cameras()
+        if(cams):
+            print(f"Camera Found:{cams}")
+            return cams
+        raise FileNotFoundError("No cameras found")
+        
 def get_frame(camera):
-    with camera:
-        frame = camera.get_frame()
-        frame.convert_pixel_format(PixelFormat.Mono8)
+    with vimba:
+        with camera:
+            frame = camera.get_frame()
+            frame.convert_pixel_format(PixelFormat.Mono8)
         
     return frame
 
-
-frame = get_frame(get_cams()[0])
+cams = get_cams()
+cams[0].set_access_mode(AccessMode.Full)
+frame = get_frame(cams[0])
 cv2.imwrite('test_frame.jpg',frame.as_opencv_image())
