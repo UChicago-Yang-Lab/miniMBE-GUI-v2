a8a63c5db9f39c79796945cf48374daa5a33db76
oofidc <70336313+oofidc@users.noreply.github.com>
2025-07-18 15:19:07 -0500

    camera_optimization.py modified

--- camera_optimization.py
+++ camera_optimization.py
@@ -1,4 +1,5 @@
 from vimba import *
+import cv2
 
 
 vimba = Vimba.get_instance()
@@ -6,10 +7,18 @@ vimba = Vimba.get_instance()
 def get_cams():
     cams = vimba.get_all_cameras
     if(cams):
-        print(cams)
+        print(f"Camera Found:{cams}")
         return cams
     else:
         raise FileNotFoundError
+    
+def get_frame(camera):
+    with camera:
+        frame = camera.get_frame()
+        frame.convert_pixel_format(PixelFormat.Mono8)
+        
+    return frame
 
 
-get_cams()
+frame = get_frame(get_cams()[0])
+cv2.imwrite('test_frame.jpg',frame.as_opencv_image())
