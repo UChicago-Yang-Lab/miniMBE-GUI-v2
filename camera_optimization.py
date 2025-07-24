555b539ef72c8561d3b8fcf0bed5c07937eb24e3
oofidc <70336313+oofidc@users.noreply.github.com>
2025-07-24 14:47:31 -0500

    First Prelim Version of Camera Optimization

--- camera_optimization.py
+++ camera_optimization.py
@@ -1,12 +1,22 @@
 from vimba import *
 import cv2
+import os
 import numpy as np
+import skimage.data
+from skimage.restoration import estimate_sigma
+from skimage import img_as_float
+
+#TODO:
+# - Add a warning for light instability(i.e. flashing or etc.)
 
 MIN_EXP_TIME = 1000
 MAX_EXP_TIME = 1000000
 
 MIN_GAIN = 30.0
 MAX_GAIN = 100.0
+
+
+TEMP_FOLDER = 'temp'
 
 
 
@@ -26,15 +36,43 @@ with vimba:
             
         return frame
 
+    
     def optimized_camera_values(camera):
+        #TODO: consider using expspace or logspace bcuz of scale of values.
+        
+        #Value of all weights should add up to 1
+        L_VAR_WEIGHT = 0.5
+        SIGMA_WEIGHT = 0.5 
+        image_stats = []
         with camera:
             for g in np.linspace(MIN_GAIN, MAX_GAIN, 30):
                 camera.Gain.set(g)
                 for e in np.linspace(MIN_EXP_TIME, MAX_EXP_TIME, 100):
+                    #TODO: Set up a limitation for images which are too bright or too dark
                     camera.ExposureTime.set(e)
                     frame = get_frame(camera)
-                    cv2.imwrite(f'/raw_images/gain_{g}_exp_{e}.jpg', frame.as_opencv_image())
+                    cv_frame = frame.as_opencv_image()
+                    #calculate laplacian variance of image
+                    gray_frame = cv2.cvtColor(cv_frame,cv2.COLOR_BGR2GRAY)
+                    laplacian_var = cv2.Laplacian(gray_frame,cv2.CV2_64F).var()
+                    #calculate noise using skimage's estimate noise
+                    sigma = estimate_sigma(gray_frame, average_sigmas=True)
+
+                    #Question: Should I normalize the values in each of the image stats(Laplacian Variance & Sigma)?
+                    img_score = L_VAR_WEIGHT* laplacian_var + SIGMA_WEIGHT * sigma
+
+                    #Save to dictionary
+                    image_stats.append({'gain':g,'exp_time':e,'score':img_score, 'laplacian_var':laplacian_var,'sigma':sigma})
                     print(f"Captured image with Gain: {g}, Exposure Time: {e}")
+
+        #Get the minimum score
+        min_score = min('score' for img in image_stats)
+        min_index = ['score' for img in image_stats].index(min_score)
+
+        print(f'*****\n\nOptimal Image Settings Found\nScore = {min_score}\nGain = {image_stats[min_index]['gain']}dB\nExposure_Time = {image_stats[min_index]['exp_time']}\nLap. Var & sigma= [{image_stats[min_index]['laplacian_var']},{image_stats[min_index]['sigma']}] \n\n*****')
+    
+        
+    
             
 
     def main():
